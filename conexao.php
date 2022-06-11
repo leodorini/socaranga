6f4c3a6da9c110249489ab510a8fdd1196a6c864
leodorini <61170579+leodorini@users.noreply.github.com>
2022-06-11 14:37:46 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,24 @@
+
+<?php
+	try{
+	//000webhost
+	//
+	//Banco: dbconce
+	//Admim: admin
+	//Senha: [(Sw|kBk18FykgrM
+
+
+		//LOCAL
+		//$conn = new PDO('mysql:host=localhost;dbname=bdconce','root','');
+		
+		//WEBSITE
+		//https://concessionaria-socaranga.000webhostapp.com/
+		$conn = new PDO('mysql:host=localhost;dbname=id18047533_dbconce','id18047533_admin','[(Sw|kBk18FykgrM');
+		
+		
+		//echo 'Conexão realizada com sucesso!';
+	}
+	catch(PDOException $e){
+		echo $e->getMessage();
+	}
+?>
